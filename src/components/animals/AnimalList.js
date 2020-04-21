d6b8aa2d9a49089056102faa6c7908a7b29c5522
Crystal Elsey <crystalelsey@Crystals-MacBook-Pro.local>
2020-04-21 13:13:29 -0500

    mapped through animals to find and display corresponding owner in Animal.js

--- src/components/animals/AnimalList.js
+++ src/components/animals/AnimalList.js
@@ -1,14 +1,20 @@
 import React, { useContext } from "react"
 import { AnimalContext } from "./AnimalProvider"
 import Animal from "./Animal"
+import { CustomerContext } from "../customers/CustomerProvider"
 
 export default () => {
     const { animals } = useContext(AnimalContext)
+    const { customers } = useContext(CustomerContext)
+    
     return (
         <section className="animals">
-        {
-            animals.map(anim => <Animal key={anim.id} animal={anim} />)
-        }
+            {
+                animals.map(animal => {
+                    const foundCustomer = customers.find(customer => customer.id === animal.customerId)
+                    return <Animal key={animal.id} animal={animal} customer={foundCustomer}/>
+                })
+            }
         </section>
     )
 }
