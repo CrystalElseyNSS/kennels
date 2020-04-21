57d7897ad32ec4da4354969be5e1035a5afcf049
Crystal Elsey <crystalelsey@Crystals-MacBook-Pro.local>
2020-04-21 13:37:17 -0500

    displayed foreign key data in list components

--- src/components/animals/AnimalList.js
+++ src/components/animals/AnimalList.js
@@ -2,17 +2,20 @@ import React, { useContext } from "react"
 import { AnimalContext } from "./AnimalProvider"
 import Animal from "./Animal"
 import { CustomerContext } from "../customers/CustomerProvider"
+import { LocationContext } from "../locations/LocationProvider"
 
 export default () => {
     const { animals } = useContext(AnimalContext)
     const { customers } = useContext(CustomerContext)
+    const { locations } = useContext(LocationContext)
     
     return (
         <section className="animals">
             {
                 animals.map(animal => {
                     const foundCustomer = customers.find(customer => customer.id === animal.customerId)
-                    return <Animal key={animal.id} animal={animal} customer={foundCustomer}/>
+                    const foundLocation = locations.find(location => location.id === animal.locationId)
+                    return <Animal key={animal.id} animal={animal} customer={foundCustomer} location={foundLocation}/>
                 })
             }
         </section>
--- src/components/employees/Employee.js
+++ src/components/employees/Employee.js
@@ -5,5 +5,6 @@ export default (props) => (
     <section className="employee">
         <h3 className="employee__name">Name: {props.employee.name}</h3>
         <div className="employee__position">Position: {props.employee.position}</div>
+        <div className="employee__location">Store: {props.location.name}</div>
     </section>
 )
